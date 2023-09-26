323da533e79087a95147f19854a2692e9acdb572
Iveta <eliaskova.i@gmail.com>
2023-09-26 10:12:52 +0200

    komentar

--- 1_lekce/Python1-23_ukol_1.py
+++ 1_lekce/Python1-23_ukol_1.py
@@ -1,7 +1,7 @@
 #Napiš program, který bude obsahovat jednu proměnnou jmeno. Tato proměnná bude obsahovat libovolné křestní jméno (třeba tvoje).
 #Tvůj program vytvoří e-mailovou adresu na základě této proměnné, s doménou czechitas.cz a tuto e-mailovou adresu vypíše.
 
-#Tedy pokud bude hodnota proměnné jmeno například Jindřiška, pak program vypíše Jindřiška@czechitas.cz. #
+#Tedy pokud bude hodnota proměnné jmeno například Jindřiška, pak program vypíše Jindřiška@czechitas.cz.
 
 jmeno = "Iveta"
 print(jmeno+"@czechitas.cz")
