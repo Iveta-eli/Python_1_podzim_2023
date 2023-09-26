b05bc2068d3a5d9e241156721a1c0fe1d781894d
Iveta <eliaskova.i@gmail.com>
2023-09-26 10:16:35 +0200

    úkol x

--- 1_lekce/Python1-23_ukol_1.py
+++ 1_lekce/Python1-23_ukol_1.py
@@ -1,3 +1,4 @@
+#
 #Napiš program, který bude obsahovat jednu proměnnou jmeno. Tato proměnná bude obsahovat libovolné křestní jméno (třeba tvoje).
 #Tvůj program vytvoří e-mailovou adresu na základě této proměnné, s doménou czechitas.cz a tuto e-mailovou adresu vypíše.
 
